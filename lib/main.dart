2b00e91da73a1dd9411f47e1abbcdafc2461b833
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-01-14 00:15:46 +0100

    feat: update main app structure and add testing widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:card_swipe_animation/home_page.dart';
-import 'package:card_swipe_animation/testing.dart';
+import 'package:card_swipe_animation/testing_widget/testing.dart';
+import 'package:card_swipe_animation/testing_widget/testing2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+    // const Testing(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +19,27 @@ class MyApp extends StatelessWidget {
       title: 'Card Swipe Animation',
       home: UserHomeScreen(),
       // home: HomePage(),
+    );
+  }
+}
+
+class Testing extends StatelessWidget {
+  const Testing({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Card Swipe Animation',
+      home: Scaffold(
+        body: SwipeableCardStack(
+          cards: [
+            ExampleCard(color: Colors.blue, text: 'Card 1'),
+            ExampleCard(color: Colors.red, text: 'Card 2'),
+            ExampleCard(color: Colors.green, text: 'Card 3'),
+          ],
+          // threshold: 0.3,
+        ),
+      ),
     );
   }
 }
