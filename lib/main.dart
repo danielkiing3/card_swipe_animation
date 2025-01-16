09cee77f700148d41d1ce32447f9e469a9ae7212
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-01-16 23:55:39 +0100

    refactor: Clean up the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
-import 'package:card_swipe_animation/home_page.dart';
-import 'package:card_swipe_animation/testing_widget/testing.dart';
-import 'package:card_swipe_animation/testing_widget/testing2.dart';
+import 'package:card_swipe_animation/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MyApp(),
-    // const Testing(),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,29 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Card Swipe Animation',
-      home: UserHomeScreen(),
-      // home: HomePage(),
-    );
-  }
-}
-
-class Testing extends StatelessWidget {
-  const Testing({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Card Swipe Animation',
-      home: Scaffold(
-        body: SwipeableCardStack(
-          cards: [
-            ExampleCard(color: Colors.blue, text: 'Card 1'),
-            ExampleCard(color: Colors.red, text: 'Card 2'),
-            ExampleCard(color: Colors.green, text: 'Card 3'),
-          ],
-          // threshold: 0.3,
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
